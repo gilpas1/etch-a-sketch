ff38c4f61e4bb13c50493bc4adc20a96a0015dc4
gil <gilpaskhover98@gmail.com>
2021-03-15 14:49:10 +0200

    done

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function drawBoard(num=16){
     const board = document.getElementById("board");
     for(i=1;i<=num*num;i++){
         let newDiv = document.createElement('div');
+        newDiv.classList.add("square");
         newDiv.style.border = "0.3vh solid black";
+        newDiv.addEventListener("mouseover", ()=>{
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            newDiv.style.backgroundColor = `#${randomColor}`;
+        })
         board.appendChild(newDiv);
     }
 
@@ -10,5 +15,21 @@ function drawBoard(num=16){
     board.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 }
 
+function resetBoard(){
+    //remove squares
+    const board = document.getElementById("board");
+    board.innerHTML = "";
+    //build new board
+    let newNum = -1;
+    while (!(typeof newNum == "number" && newNum <= 100 && newNum >= 0)){
+        newNum = parseInt(prompt("How many squares per side you want? (max=100)"));
+    }
+    
+    drawBoard(newNum);
+}
 
 drawBoard();
+
+const resetBtn = document.getElementById("resetButton");
+resetBtn.addEventListener('click', resetBoard);
+
